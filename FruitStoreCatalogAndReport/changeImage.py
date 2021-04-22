4eb5df34c5c53624b9932b529f71432d0f3c6ee5
Matthew Glassman <mglassmn@email.unc.edu>
2021-04-22 19:40:49 -0400

    Created the basic logic for image manipulation and posting JPEG to web server.

--- FruitStoreCatalogAndReport/changeImage.py
+++ FruitStoreCatalogAndReport/changeImage.py
@@ -8,7 +8,17 @@ from pathlib import Path
 from PIL import Image
 
 #TODO Set size
+size = (400, 600)
 
 #TODO Set input and output
+for infile in os.listdir("<path to files>"):						#For files in path
+	outfile = os.path.splitext(infile) [0]		#Get file and extension
+
 
 #Try Opening and converting file from TIF to JPEG and save
+	try:
+		with Image.open(infile).convert('RGB') as im:
+			im.thumbnail(size)
+			im.save("<path_to_be>" + outfile, "JPEG")
+	except OSError:
+		pass	
--- FruitStoreCatalogAndReport/supplier_image_upload.py
+++ FruitStoreCatalogAndReport/supplier_image_upload.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
-import request
+import requests
 import os
 import path
 
 #TODO Traverse files in path
+path = "<path_to_photo_files>"  #EX. /home/<user>/images/
+
+url = "http://localhost/upload/"
+
+folder = os.listdir(path)
+for imagefile in folder:
+	with open(path + imagefile, 'rb') as image: #Read as binary
+	
 
 #TODO Upload files to online location
+	r = requests.post(url, files={'file': image})
