fd17dffda14449f09c324afb3a2ba02b628d8674
Matthew Glassman <matthewglassman78@gmail.com>
2021-05-04 10:10:27 -0400

    Created variables to hold pieces of the date to use in the title of PDF report.

--- FruitStoreCatalogAndReport/report_email.py
+++ FruitStoreCatalogAndReport/report_email.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 
+#Import Modules needed
+import reports
+import emails
+import datetime
+
 #Will send use reports.py and emails.py to create the actual PDF and email to send out
+
+#Create date to pass to title
+
+#First get the whole date into a variable
+currentdate = datetime.datetime.now()
+
+#Pull out each part of the date needed
+currentmonth = currentdate.strftime("%B")
+currentday = currentdate.strftime("%d")
+currentyear = currentdate.strftime("%Y")
+
+def main(argv):
+	#Generate PDF Report
+	title = "Processed Update on " + 
