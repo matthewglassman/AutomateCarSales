9a57be4f9d9a18115fae6c386a26161874ab6fdf
Matthew Glassman <matthewglassman78@gmail.com>
2021-05-12 20:37:19 -0400

    Adding a variable to hold path to text to parse. Assigning keys for a dictionary to serialize to JSON.

--- FruitStoreCatalogAndReport/run.py
+++ FruitStoreCatalogAndReport/run.py
@@ -6,10 +6,14 @@ import requests
 import os
 import path
 
-#Will convert text files to dictionary and then into JSON to send to web
+"""Will convert text files to dictionary and then into JSON to send to web"""
 
 #TODO Create path to text files and traverse through files adding entries to a dictionary
+path = "<path to text files to iterate>"
 
-#Convert Dictionary to JSON Object
+keys = ["name", "weight", "description", "image_name"]
+
+
+#Iterate over the objects and build a dictionary
 
 #POST JSON Object to onlne location
